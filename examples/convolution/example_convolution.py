4ce2b7a185d28609cb84cd654851ac91da93b171
Wenhao Xie <wh.xie@outlook.com>
2025-05-10 13:23:11 +0800

    [CI] Add Convolution example to CI (#473)
    * add convolution example to CI
    
    * lint fix
    
    * Update test_example_convolution.py
    
    ---------
    
    Co-authored-by: Lei Wang <34334180+LeiWang1999@users.noreply.github.com>

--- examples/convolution/example_convolution.py
+++ examples/convolution/example_convolution.py
@@ -232,7 +232,7 @@ def convolution(N,
     return main
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--n', type=int, default=128, help='n')
     parser.add_argument('--c', type=int, default=128, help='c')
@@ -271,3 +271,7 @@ if __name__ == "__main__":
     out_c = kernel(a, b)
     ref_c = ref_program(S, P, D)(a, b)
     torch.testing.assert_close(out_c, ref_c, rtol=1e-2, atol=1e-2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/convolution/test_example_convolution.py
@@ -0,0 +1,14 @@
+# Copyright (c) Tile-AI Corporation.
+# Licensed under the MIT License.
+import tilelang.testing
+
+import example_convolution
+
+
+@tilelang.testing.requires_cuda
+def test_example_convolution():
+    example_convolution.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
